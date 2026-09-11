6c0fd7e2e39860d77a7c60164c7869fe38c01a51
andrerezendebr <andrerezende_br@yahoo.com.br>
2026-09-11 11:01:27 -0300

    Ajusta escala de trabalho na coordenação

--- app/static/sw-register.js
+++ app/static/sw-register.js
@@ -11,7 +11,7 @@
   });
 
   navigator.serviceWorker
-    .register("/sw.js?v=91")
+    .register("/sw.js?v=92")
     .then((reg) => {
       console.log("SW registrado:", reg.scope);
       // Verifica atualização a cada carregamento
--- app/static/sw.js
+++ app/static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "appEventos-v153";
+const CACHE = "appEventos-v154";
 const SHELL = [
   "/static/style.css",
   "/static/app.js",
